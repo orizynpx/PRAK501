59847f4c0b7a8a05eb5c139821fe01ed314922af
orizynpx <71197340+orizynpx@users.noreply.github.com>
2026-05-25 12:23:02 +0800

    feat: init fork

--- Modul1/PRAK101.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $nama = "Arya Arrozza Ridho Syaputra";
-    $nim = 2410817210010;
-
-    echo "Hello World in PHP<br>";
-    echo "Nama Praktikan : $nama<br>";
-    echo "NIM : $nim";
-?>
