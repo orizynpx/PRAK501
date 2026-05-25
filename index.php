4084689e2ae7d40ca1f336dd82d72e93360fe9e5
orizynpx <71197340+orizynpx@users.noreply.github.com>
2026-05-26 01:34:16 +0800

    refactor: change internal CSS to external CSS

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Perpustakaan</title>
+    <link rel="stylesheet" href="style.css">
     <style>
     /* --- Reset & Base Styles --- */
     * {
